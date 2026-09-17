ce3eb0d0c4a210799ddac5afff5bd8f2374f6b23
oups1004-sketch <oups1004@gmail.com>
2026-09-17 15:36:17 +0900

    Apply counselor portrait section styling

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./header-refresh.css";
 import "./site-header.css";
 import "./polish.css";
 import "./story-balance.css";
+import "./counselor-portrait.css";
 
 export const metadata: Metadata = {
   title: "아우데 심리상담 | 용기를 내어, 당신의 이야기를",
